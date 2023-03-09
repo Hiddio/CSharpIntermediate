f996adee6cca4189a48323620c72e2db42b04d72
Hiddio <simon.verhoeff205@gmail.com>
2023-03-09 19:32:29 +0100

    Updated Enemy
    Added Enemy.Spawn Function

--- MonoGameTest/MonoGameTest/Enemy.cs
+++ MonoGameTest/MonoGameTest/Enemy.cs
@@ -14,11 +14,18 @@ namespace MonoGameTest
         public Texture2D _enemyTexture;
         public float _scale;
         Rectangle hitBox;
+
+        
         public Enemy(Vector2 pos, float scale)
         {
             _position = pos;
             _scale = scale;
             hitBox = new Rectangle((int)_position.X, (int)_position.Y, _enemyTexture.Width, _enemyTexture.Height);
+        }
+
+        public void Spawn()
+        {
+
         }
 
         public void Update()
