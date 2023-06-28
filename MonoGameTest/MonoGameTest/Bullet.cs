fad465a0ed4b36420bd339b87009f5804dd9dd12
Hiddio <simon.verhoeff205@gmail.com>
2023-06-28 11:08:57 +0200

    Update Bullet.cs

--- MonoGameTest/MonoGameTest/Bullet.cs
+++ MonoGameTest/MonoGameTest/Bullet.cs
@@ -12,7 +12,7 @@ namespace MonoGameTest
         public Texture2D Texture;
         public float Scale;
         public Rectangle HitBox;
-        
+        public List<Enemy> currentEnemies;
         public Bullet(Vector2 pos, float scale, Texture2D texture)
         {
             Console.WriteLine("BAM!");
@@ -20,8 +20,10 @@ namespace MonoGameTest
             Position = pos;
 
             Scale = scale;
-            
+
             Texture = texture;
+
+            SetHitbox();
         }
 
         public void SetHitbox()
@@ -29,21 +31,32 @@ namespace MonoGameTest
             HitBox = new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
         }
 
+        public void EnemyCollision()
+        {
+            foreach (Enemy enemy in currentEnemies)
+            {
+                if (enemy.HitBox.Contains(this.HitBox))
+                {
+
+                }
+            }
+        }
+
         public void Update()
         {
             //FEEDBACK Ik raad je aan om de Bullet een richting (direction) te geven i.p.v. deze boolean
             //FEEDBACK Voor de Player kun je dan new Vector2(0,-10) gebruiken en voor de Enemy new Vector2(0,10)
             //FEEDBACK Het mooiste is om dit aan de constructor toe te voegen
-            
+
             //FEEDBACK Op deze manier kun je de Bullet class makkelijker in verschillende scenarios gebruiken
             //FEEDBACK Dan staat de Bullet class los van de Player class en de Enemy class
-            
-            
+
+
             Position.Y -= 10;
             // edit, weggehaald "enemy bool check" voor else als enemy bullet schoot
-            
+
         }
-       
+
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(Texture, Position, null, Color.White, 0, Vector2.Zero, Scale, SpriteEffects.None, 0);
